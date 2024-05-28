4f79fe32eb2b87635c93b65311c165eab865ce88
svadr <salvadorbeltran.dev@gmail.com>
2024-05-28 14:53:20 -0700

    keymapsv

--- lua/svadr/core/keymaps.lua
+++ lua/svadr/core/keymaps.lua
@@ -16,3 +16,5 @@ keymap.set("n", "<leader>bj", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>bn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>bm", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>by", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
